6eb797eded920e4676f8f7f6ae9e5aae55919191
HebaElgohary <hebasamir535@gmial.com>
2025-03-03 19:25:14 +0200

    before pull

--- src/screens/MainScreen.jsx
+++ src/screens/MainScreen.jsx
@@ -1,17 +1,26 @@
 import 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import { Provider as PaperProvider } from 'react-native-paper';
+
 import {Home} from './Home';
+import {AddMedicine} from './AddMedicine';  
+import {Donations} from './Donations';
 const MainScreen = () => {
     const Stack = createStackNavigator();
 
     return (
-    
+        <PaperProvider>
+
     <NavigationContainer>
-        <Stack.Navigator initialRouteName="Home">
+        <Stack.Navigator initialRouteName="Donations">
             <Stack.Screen name="Home" component={Home} />
+            <Stack.Screen name="AddMedicine" component={AddMedicine} />
+            <Stack.Screen name="Donations" component={Donations} />
         </Stack.Navigator>
     </NavigationContainer>
+    </PaperProvider>
+
   )
 }
 
